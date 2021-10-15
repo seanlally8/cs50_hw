c444d02e0faba7ad062353e7f14ad84009d52d17
seanlally8 <seanlally8@gmail.com>
2021-10-15 18:59:35 +0000

    embedded two loops in one large loop, successful except for check50

--- pset4/recover/recover.c
+++ pset4/recover/recover.c
@@ -10,6 +10,7 @@ int filename = 0;
 
 char filedigits[7];
 
+int jpgcheck(BYTE *buffer);
 
 int main(int argc, char *argv[])
 {
@@ -22,25 +23,61 @@ int main(int argc, char *argv[])
     FILE *source = fopen(argv[1], "r");
     if (source == NULL)
     {
+        fclose(source);
         printf("Unable to open file");
         return 1;
     }
 
 
     BYTE *buffer = malloc(sizeof(BYTE) * BLOCK);
-    sprintf(filedigits, "%.3i.jpg", filename);
-
-    while (fread(buffer, sizeof(BYTE), BLOCK, source))
+    if (buffer == NULL)
     {
-        if ((buffer[0] == 0xff &&  buffer[1] == 0xd8 && buffer [2] == 0xff) && (buffer[3] > 0xdf && buffer[3] < 0xf0))
+        free(buffer);
+        return 1;
+    }
+
+    while (fread(buffer, sizeof(BYTE), BLOCK, source) != EOF)
+    {
+        while(fread(buffer, sizeof(BYTE), BLOCK, source))
         {
-            FILE *newfile = fopen(filedigits, "w");
-            while (fread(buffer, sizeof(BYTE), BLOCK, source))
+            if (jpgcheck(buffer) == 1)
             {
-                fwrite(buffer, sizeof(BYTE), BLOCK, newfile);
+                break;
             }
-            fclose(newfile);
         }
+
+        sprintf(filedigits, "%.3i.jpg", filename);
+        FILE *newfile = fopen(filedigits, "w");
         filename++;
+
+        while (fwrite(buffer, sizeof(BYTE), BLOCK, newfile))
+        {
+            fread(buffer, sizeof(BYTE), BLOCK, source);
+            if (jpgcheck(buffer) == 1)
+            {
+                fclose(newfile);
+                sprintf(filedigits, "%.3i.jpg", filename);
+                newfile = fopen(filedigits, "w");
+                filename++;
+            }
+        }
+        fclose(newfile);
+    }
+    fclose(source);
+    free(buffer);
+    free(filedigits);
+    return 0;
+}
+
+int jpgcheck(BYTE *buffer)
+{
+    if ((buffer[0] == 0xff &&  buffer[1] == 0xd8 && buffer [2] == 0xff) && (buffer[3] > 0xdf && buffer[3] < 0xf0))
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
     }
 }
+
