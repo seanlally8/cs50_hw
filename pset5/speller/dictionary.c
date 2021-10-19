8ff82ad4b3f1b18caaee56de045b33cec3d23e33
seanlally8 <seanlally8@gmail.com>
2021-10-19 00:12:28 +0000

    completed speller

--- pset5/speller/dictionary.c
+++ pset5/speller/dictionary.c
@@ -5,6 +5,8 @@
 #include <ctype.h>
 #include <stdlib.h>
 #include <string.h>
+#include <strings.h>
+#include <cs50.h>
 
 #include "dictionary.h"
 
@@ -19,8 +21,17 @@ typedef struct node
 }
 node;
 
+// Prototype free_l function
+bool free_l(node *head);
+
+// Assign bool variable for determining whether load succeeded
+bool loaded = false;
+
 // Number of buckets in hash table
 const unsigned int N = 10007;
+
+// Designate global counter for size function
+unsigned int counter = 0;
 
 // Hash table
 node *table[N];
@@ -28,28 +39,47 @@ node *table[N];
 // Returns true if word is in dictionary, else false
 bool check(const char *word)
 {
-    // TODO
+    // Assign return value of hash function to a variable for convenience
+    unsigned int index = hash(word);
+
+    // Generate search pointer initialized to the first element in linked list at hash/index
+    node *search = table[index];
+
+    //Search list for word by incrementing across the elements
+    while (search != NULL)
+    {
+        int i = strcasecmp(word, search->word);
+        if (i == 0)
+        {
+            return true;
+        }
+
+        else if (i != 0)
+        {
+            search = search->next;
+        }
+    }
+
+    // If we've made it through the previous loop without returning true, it means we haven't found the word and we can return false
     return false;
 }
 
-// This hash function is taken from James Aspnes' (Professor of CS at Yale) notes
-// at https://www.cs.yale.edu/homes/aspnes/pinewiki/C(2f)HashTables.html?highlight=%28CategoryAlgorithmNotes%29
+/*A case-insensitive implementation of the djb2 hash function.
+
+ can be found on github: https://github.com/hathix/cs50-section/blob/master/code/7/sample-hash-functions/good-hash-function.c
+
+ Adapted by Neel Mehta from
+ http://stackoverflow.com/questions/2571683/djb2-hash-function. */
 unsigned int hash(const char *word)
 {
-    unsigned long h;
-    unsigned const char *us;
+    unsigned long hash = 5381;
 
-    /* cast s to unsigned const char * */
-    /* this ensures that elements of s will be treated as having values >= 0 */
-    us = (unsigned const char *) word;
-
-    h = 0;
-    while(*us != '\0') {
-        h = (h * BASE + *us) % N;
-        us++;
+    for (const char *ptr = word; *ptr != '\0'; ptr++)
+    {
+        hash = ((hash << 5) + hash) + tolower(*ptr);
     }
 
-    return h;
+    return hash % N;
 }
 
 // Loads dictionary into memory, returning true if successful, else false
@@ -58,10 +88,16 @@ bool load(const char *dictionary)
     // Declare buffer into which we'll temporarily store each word
     char buffer[LENGTH + 1];
 
+    // Initialize table[] to NULL
+    for (int i = 0; i < N; i++)
+    {
+        table[i] = NULL;
+    }
+
     // Open the dictionary so we can read out of it
     FILE *dict = fopen(dictionary, "r");
 
-    //Check to make sure the file opens
+    // Check to make sure the file opens
     if (dict == NULL)
     {
         return false;
@@ -70,31 +106,59 @@ bool load(const char *dictionary)
     // Read each word out of the dictionary into the table, using the hash function defined above
     while ((fscanf(dict, "%s", buffer)) != EOF)
     {
-        table[hash(buffer)] = malloc(sizeof(node));
-        strcpy(table[hash(buffer)]->word, buffer);
+        // Assign return value of hash function to variable for convenience
+        unsigned int index = hash(buffer);
+
+        // Add our first node to the appropriate index (as indicated by hash function)
+        if (table[index] == NULL)
+        {
+            node *tmp = malloc(sizeof(node));
+            if (tmp == NULL)
+            {
+                fclose(dict);
+                loaded = false;
+                return false;
+            }
+
+            strcpy(tmp->word, buffer);
+            tmp->next = NULL;
+
+            table[index] = tmp;
+        }
+
+        // Add subsequent nodes to the beginning of list
+        else
+        {
+            node *tmp1 = malloc(sizeof(node));
+            if (tmp1 == NULL)
+            {
+                fclose(dict);
+                loaded = false;
+                return false;
+            }
+
+            strcpy(tmp1->word, buffer);
+            tmp1->next = table[index];
+
+            table[index] = tmp1;
+        }
+
+        // Count the number of words being added to hash table (for size function)
+        counter++;
     }
 
-    //If we've reached the end of the file, we've succeeded, so we can return true
-    if (fscanf(dict, "%s", buffer) == EOF)
-    {
-        return true;
-    }
-
-    //Otherwise, we've failed, so return false
-    else
-    {
-        return false;
-    }
+    fclose(dict);
+    loaded = true;
+    return true;
 }
 
 // Returns number of words in dictionary if loaded, else 0 if not yet loaded
 unsigned int size(void)
 {
-    // Note todo: perhaps write a simple count function and plug it into load,
-    // so as we load the hash table, we can count the words
-    if (load(dictionary) == 1)
+    // This takes the value of counter, incremented in load, and returns that value (if dict is loaded) -- otherwise returning 0
+    if (loaded)
     {
-
+        return counter;
     }
 
     else
@@ -106,6 +170,41 @@ unsigned int size(void)
 // Unloads dictionary from memory, returning true if successful, else false
 bool unload(void)
 {
-    // TODO
-    return false;
+    //
+    bool count = false;
+
+    for (int i = 0; i < N; i++)
+    {
+        if (free_l(table[i]) == 1)
+        {
+            count = true;
+        }
+
+    }
+
+    if (count == 1)
+    {
+        return true;
+    }
+
+    else
+    {
+        return false;
+    }
+}
+
+bool free_l(node *head)
+{
+    if (head == NULL)
+    {
+        return true;
+    }
+
+    else
+    {
+        free_l(head->next);
+        free(head);
+    }
+
+    return true;
 }
