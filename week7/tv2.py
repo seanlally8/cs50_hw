367061ea05b933fd770f9e24a5406854469d04b5
seanlally8 <seanlally8@gmail.com>
2021-10-28 03:15:14 +0000

    finished lab7

--- week7/tv2.py
+++ week7/tv2.py
@@ -1,13 +1,26 @@
 import csv
+from cs50 import SQL
 import sys
+
+# we can also use the touch command in cli to make file
+open("shows.db", "w").close()
+
+# Opens otherwise empty file above,
+db = SQL("sqlite:///shows.db")
+
+#Creates the two tables from notes
+db.execute("CREATE TABLE shows (id Integer, title TEXT, PRIMARY KEY(id))")
+db.execute("CREATE TABLE genres (show_id INTEGER, genre TEXT, FOREIGN(show_id) REFERENCES shows(id)")
+
 
 title = input("Title: ").strip().upper()
 
-with open(sys.argv[1], "r") as file:
+with open(sys.argv[2], "r") as file:
     reader = csv.DictReader(file)
-    counter = 0
     for row in reader:
-        if row["Title"].strip().upper() == title:
-            counter += 1
+        title = row["title"].strip().upper()
 
-print(counter)
+        # ? is same as %s in C or {} in Python it turns out libraries will often return a value for methods like .execute below
+        id = db.execute("INSERT INTO shows (title) VALUES(?)", title)
+        for genre in row["genres"].split(", "):
+            db.execute("INSERT INTO genres (show_id, genre) VALUES(?, ?)", id, genre)
