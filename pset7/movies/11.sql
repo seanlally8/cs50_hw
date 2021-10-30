cba6ae459eb840406d1bcab2829a50d1ff0eb963
Sean <seanlally8@gmail.com>
2021-10-30 14:46:37 -0700

    completed 11.sql started 12.sql

--- pset7/movies/11.sql
+++ pset7/movies/11.sql
@@ -1,4 +1,1 @@
-SELECT title FROM movies WHERE id IN (SELECT movie_id FROM stars WHERE person_id IN (SELECT id FROM people WHERE name = "Chadwick Boseman"));
-
-SELECT title FROM movies WHERE id IN (SELECT movie_id FROM ratings ORDER BY rating) AND id IN (SELECT movie_id FROM stars WHERE person_id IN (SELECT id FROM people WHERE name = "Chadwick Boseman")) LIMIT 5;
-
+SELECT title FROM people JOIN stars ON stars.person_id = people.id JOIN movies ON stars.movie_id = movies.id JOIN ratings ON ratings.movie_id = movies.id WHERE name = "Chadwick Boseman" ORDER BY rating DESC LIMIT 5;
