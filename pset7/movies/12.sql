43641f3b4b4bb5d6549a5d2606c8883dcc9eb107
seanlally8 <seanlally8@gmail.com>
2021-10-31 19:21:43 +0000

    finished movies

--- pset7/movies/12.sql
+++ pset7/movies/12.sql
@@ -1,2 +1,6 @@
-SELECT title FROM people JOIN stars ON people.id = stars.person_id JOIN movies ON stars.movie_id = movies.id WHERE name = "Johnny Depp";
-
+SELECT title FROM people
+JOIN stars ON people.id = stars.person_id
+JOIN movies ON stars.movie_id = movies.id
+WHERE name IN ("Johnny Depp", "Helena Bonham Carter")
+GROUP BY title
+HAVING COUNT(title) > 1;
